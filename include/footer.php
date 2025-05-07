0acdaf5d5fc0a21cf898aeed32423186e6e33d34
Santhoshkumar T M <santhoshsandy555tm@gmail.com>
2025-05-07 11:42:30 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,15 @@
+<footer class="">
+		<div class="container">
+			
+			<div class="footer-bottom">
+				<div class="row">
+					
+					<div class="col-md-8 text-md-right">
+						<div class="copyright"> 
+							<centar>GYM Managaement System</centar>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
